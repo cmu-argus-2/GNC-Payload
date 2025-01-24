cc878dfbcfeb04f79dc5d33f72b59dda19402fb7
Amaar Quadri <amaarquadri@gmail.com>
2025-01-23 23:58:22 -0500

    increase readability

--- image_downloader/eedl_16_regions.sh
+++ image_downloader/eedl_16_regions.sh
@@ -2,5 +2,17 @@
 
 regions=("10S" "10T" "11R" "12R" "16T" "17R" "17T" "18S" "32S" "32T" "33S" "33T" "52S" "53S" "54S" "54T")
 for region in "${regions[@]}"; do
-  python3 eedl.py -g "$region" -i 2022 -f 2023-06-15 -s 500 -vb 250000 -hb 250000 -o "${region}" -se l9 -m 30 -c EPSG:4326 -rm True -gd True
+  python3 eedl.py \
+    --grid_key "$region" \
+    --idate 2022 \
+    --fdate 2023-06-15 \
+    --scale 500 \
+    --vertical_buffer 250000 \
+    --horizontal_buffer 250000 \
+    --outpath "${region}" \
+    -sensor l9 \
+    --maxims 30 \
+    --crs EPSG:4326 \
+    -region_mosaic True \
+    --gdrive True
 done
