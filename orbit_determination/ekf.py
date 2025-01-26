ac8e8554b61e264884f4fa3bca80224b90114213
frede791 <frederik.anilmarkus@gmail.com>
2025-01-26 12:52:04 -0500

    auto formatting
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- orbit_determination/ekf.py
+++ orbit_determination/ekf.py
@@ -9,19 +9,21 @@ import yaml
 from dynamics.orbital_dynamics import f_jac
 from vision_inference.landmark_bearings import LandmarkBearingSensor
 
+
 class EKF:
     """
     Extended Kalman Filter
     """
 
-    def __init__(self, 
-                 x: np.ndarray,
-                 P: np.ndarray,
-                 Q: np.ndarray,
-                 R: np.ndarray,
-                 att_quat: np.ndarray,
-                 dt: float
-                 ) -> None:
+    def __init__(
+        self,
+        x: np.ndarray,
+        P: np.ndarray,
+        Q: np.ndarray,
+        R: np.ndarray,
+        att_quat: np.ndarray,
+        dt: float,
+    ) -> None:
         """
         Initialize the EKF
 
@@ -34,7 +36,7 @@ class EKF:
         """
         self.x_m = x
         self.x_p = x
-        self.att_quat = att_quat # TODO: Merge with MEKF to update attitude quaternion
+        self.att_quat = att_quat  # TODO: Merge with MEKF to update attitude quaternion
         self.P_m = P
         self.P_p = P
         self.Q = Q
@@ -49,7 +51,6 @@ class EKF:
         self.landmark_bearing_sensor = LandmarkBearingSensor(config)
         # TODO: Get the actual images into the EKF.
 
-
     def predict(self) -> None:
         """
         Predict the next prior state. This corresponds to the prior update step in the EKF algorithm.
@@ -58,8 +59,7 @@ class EKF:
         self.x_p = A @ self.x_m
         self.P_p = A @ self.P_m @ A.T + self.Q
 
-
-    def measurement(self, z: Tuple[np.ndarray,np.ndarray]) -> None:
+    def measurement(self, z: Tuple[np.ndarray, np.ndarray]) -> None:
         """
         Update the state estimate based on the measurement. This corresponds to the posterior update step in the EKF algorithm.
 
@@ -71,15 +71,16 @@ class EKF:
 
         S = H @ self.P_p @ H.T + self.R
         cond = np.linalg.cond(S)
-        
+
         # Check for ill-conditioned matrix and add regularization if necessary
         if cond > self.cond_threshold:
             S += np.eye(S.shape[0]) * 1e-6
 
         K = self.P_p @ H.T @ np.linalg.inv(S)
         self.x_m = self.x_p + K @ (z - h)
-        self.P_m = (np.eye(self.P_m.shape[0]) - K @ H) @ self.P_p @ (np.eye(self.P_m.shape[0]) - K @ H).T + K @ self.R @ K.T # Joseph form covariance update
-
+        self.P_m = (np.eye(self.P_m.shape[0]) - K @ H) @ self.P_p @ (
+            np.eye(self.P_m.shape[0]) - K @ H
+        ).T + K @ self.R @ K.T  # Joseph form covariance update
 
     def H(self, z: np.ndarray, x_p: np.ndarray) -> np.ndarray:
         """
@@ -92,7 +93,6 @@ class EKF:
 
         jac = jax.jacobian(self.h, argnums=1)(z, x_p)
         return jac
-
 
     def h(self, z: np.ndarray, x_p: np.ndarray) -> np.ndarray:
         """
@@ -109,7 +109,7 @@ class EKF:
             vec /= jnp.linalg.norm(vec)
             body_R_eci = Rotation.from_quat(self.att_quat, scalar_first=True).as_matrix()
             body_vec = body_R_eci @ vec
-            estimate[i,:] = body_vec
+            estimate[i, :] = body_vec
         return estimate
 
 
@@ -127,4 +127,3 @@ def load_config() -> dict[str, Any]:
     config["mission"]["duration"] = 3 * 90 * 60  # s, roughly 1 orbit
 
     return config
-
