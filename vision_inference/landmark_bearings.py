171d9ff6398436a96a2b5cbe28fa7e3924ebae3d
frede791 <frederik.anilmarkus@gmail.com>
2025-01-26 12:52:04 -0500

    auto formatting
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- vision_inference/landmark_bearings.py
+++ vision_inference/landmark_bearings.py
@@ -21,13 +21,18 @@ class LandmarkBearingSensor:
         """
         :param config: The configuration dictionary.
         """
-        camera_Q_body = np.asarray(config["satellite"]["camera"]["orientation_in_cubesat_frame"])
-        self.body_R_camera = Rotation.from_quat(camera_Q_body, scalar_first=True).inv().as_matrix()
+        camera_Q_body = np.asarray(
+            config["satellite"]["camera"]["orientation_in_cubesat_frame"]
+        )
+        self.body_R_camera = (
+            Rotation.from_quat(camera_Q_body, scalar_first=True).inv().as_matrix()
+        )
         self.ml_pipeline = MLPipeline()
         self.earth_image_simulator = EarthImageSimulator()
 
-    def take_measurement(self, epoch: Epoch, cubesat_position: np.ndarray, eci_R_body: np.ndarray) \
-            -> Tuple[np.ndarray, np.ndarray]:
+    def take_measurement(
+        self, epoch: Epoch, cubesat_position: np.ndarray, eci_R_body: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray]:
         """
         Take a set of landmark bearing measurements.
 
@@ -57,13 +62,11 @@ class LandmarkBearingSensor:
         regions_and_landmarks = self.ml_pipeline.run_ml_pipeline_on_single(frame)
 
         # save the image with the detected landmarks
-        epoch_str = str(epoch) \
-            .replace(':', '_') \
-            .replace(' ', '_') \
-            .replace('.', '_')
+        epoch_str = str(epoch).replace(":", "_").replace(" ", "_").replace(".", "_")
         output_dir = os.path.abspath(
             # TODO: Determine path for actual imagery
-            os.path.join(__file__, f"../log/images/seed_69420_epoch_{epoch_str}/"))
+            os.path.join(__file__, f"../log/images/seed_69420_epoch_{epoch_str}/")
+        )
         os.makedirs(output_dir, exist_ok=True)
         self.ml_pipeline.visualize_landmarks(frame, regions_and_landmarks, output_dir)
 
@@ -72,18 +75,30 @@ class LandmarkBearingSensor:
         confidence_scores = np.zeros(shape=(0,))
 
         for _, landmarks in regions_and_landmarks:
-            centroids_ecef = lat_lon_to_ecef(landmarks.centroid_latlons[np.newaxis, ...]).reshape(-1, 3)
+            centroids_ecef = lat_lon_to_ecef(
+                landmarks.centroid_latlons[np.newaxis, ...]
+            ).reshape(-1, 3)
 
-            landmark_positions_ecef = np.concatenate((landmark_positions_ecef, centroids_ecef), axis=0)
-            pixel_coordinates = np.concatenate((pixel_coordinates, landmarks.centroid_xy), axis=0)
-            confidence_scores = np.concatenate((confidence_scores, landmarks.confidence_scores), axis=0)
+            landmark_positions_ecef = np.concatenate(
+                (landmark_positions_ecef, centroids_ecef), axis=0
+            )
+            pixel_coordinates = np.concatenate(
+                (pixel_coordinates, landmarks.centroid_xy), axis=0
+            )
+            confidence_scores = np.concatenate(
+                (confidence_scores, landmarks.confidence_scores), axis=0
+            )
 
         if len(confidence_scores) == 0:
             print("No landmarks detected")
             return np.zeros(shape=(0, 3)), np.zeros(shape=(0, 3))
 
         landmark_positions_eci = (ecef_R_eci.T @ landmark_positions_ecef.T).T
-        bearing_unit_vectors_cf = self.earth_image_simulator.camera.pixel_to_bearing_unit_vector(pixel_coordinates)
+        bearing_unit_vectors_cf = (
+            self.earth_image_simulator.camera.pixel_to_bearing_unit_vector(
+                pixel_coordinates
+            )
+        )
         bearing_unit_vectors = (self.body_R_camera @ bearing_unit_vectors_cf.T).T
 
         print(f"Detected {len(landmark_positions_eci)} landmarks")
