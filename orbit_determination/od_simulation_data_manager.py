691fc07fa43d17d421d43745112e3a0e3b0d0960
frede791 <frederik.anilmarkus@gmail.com>
2025-02-10 12:59:46 -0500

    fixes to various files rebased on main
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- orbit_determination/od_simulation_data_manager.py
+++ orbit_determination/od_simulation_data_manager.py
@@ -36,6 +36,9 @@ class ODSimulationDataManager:
     Rs_body_to_eci: np.ndarray = field(default_factory=lambda: np.zeros(shape=(0, 3, 3)))
 
     measurement_indices: np.ndarray = field(default_factory=lambda: np.array([], dtype=int))
+    bearing_unit_vectors: np.ndarray = field(default_factory= lambda: np.zeros(shape=(0, 3)))
+    landmarks: np.ndarray = field(default_factory=lambda: np.zeros(shape=(0, 3)))
+    measurement_indices: np.ndarray = field(default_factory=lambda: np.array([], dtype=int))
     bearing_unit_vectors: np.ndarray = field(default_factory=lambda: np.zeros(shape=(0, 3)))
     landmarks: np.ndarray = field(default_factory=lambda: np.zeros(shape=(0, 3)))
 
