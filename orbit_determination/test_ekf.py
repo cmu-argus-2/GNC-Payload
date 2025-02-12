8a5531a2027778f1c491c2584f7e5d818f7835fb
frede791 <frederik.anilmarkus@gmail.com>
2025-02-12 12:25:50 -0500

    functioning and convergent EKF
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- orbit_determination/test_ekf.py
+++ orbit_determination/test_ekf.py
@@ -5,34 +5,33 @@ Testing the EKF class.
 import os
 import sys
 from time import time
-from typing import Any
-from typing import List
+from typing import Any, List
+
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import brahe
-from brahe.constants import GM_EARTH
-from brahe.constants import R_EARTH
-from brahe.epoch import Epoch
-import numpy as np
 import pickle
-import yaml
 
-from dynamics.orbital_dynamics import f
-from dynamics.orbital_dynamics import f_jac
-from orbit_determination.ekf import EKF
-from orbit_determination.landmark_bearing_sensors import SimulatedMLLandmarkBearingSensor
-from orbit_determination.landmark_bearing_sensors import GroundTruthLandmarkBearingSensor
-from orbit_determination.od_simulation_data_manager import ODSimulationDataManager
+import brahe
+import numpy as np
 import quaternion
-from sensors.imu import IMU
-from sensors.sensor import SensorNoiseParams
+import yaml
+from brahe.constants import GM_EARTH, R_EARTH
+from brahe.epoch import Epoch
+
+from dynamics.orbital_dynamics import f, f_jac
+from orbit_determination.ekf import EKF
+from orbit_determination.landmark_bearing_sensors import (
+    GroundTruthLandmarkBearingSensor,
+    SimulatedMLLandmarkBearingSensor,
+)
+from orbit_determination.od_simulation_data_manager import ODSimulationDataManager
 from sensors.bias import BiasParams
-from sensors.imu import IMUNoiseParams
+from sensors.imu import IMU, IMUNoiseParams
+from sensors.sensor import SensorNoiseParams
 from utils.config_utils import load_config
-from utils.orbit_utils import get_sso_orbit_state
-from utils.orbit_utils import is_over_daytime
+from utils.orbit_utils import get_sso_orbit_state, is_over_daytime
 
 
 def imu_init(dt):
@@ -88,10 +87,11 @@ def run_simulation():
     # Initialize IMU and EKF
     imu = imu_init(dt)
     ekf = EKF(
-        r=initial_state[0:3] + np.random.normal(0, 10, 3),  # TODO: Adjust and tune noise and error init
-        v=initial_state[3:6] + np.random.normal(0, 10, 3),
+        r=initial_state[0:3]
+        + np.random.normal(0, 800, 3),  # TODO: Adjust and tune noise and error init
+        v=initial_state[3:6] + np.random.normal(0, 800, 3),
         q=quaternion.from_rotation_matrix(init_rot),
-        P=np.eye(6) * 10,
+        P=np.eye(6) * 1000,
         Q=np.eye(6) * 1e-12,
         R=np.zeros((3, 3)),
         dt=dt,
@@ -114,11 +114,11 @@ def run_simulation():
             np.expand_dims(quaternion.as_rotation_matrix(next_quat), axis=0),
         )
 
-        gyro_meas = np.zeros((3)) # TEMPORARY 
+        gyro_meas = np.zeros((3))  # TEMPORARY
         # gyro_meas, _ = imu.update(quaternion.as_rotation_vector(next_quat), [0, 0, 0])
         ekf.predict(u=gyro_meas)
 
-        if t % 10 == 0:
+        if t % 1 == 0:
             data_manager.take_measurement(landmark_bearing_sensor)
             print(f"Total measurements so far: {data_manager.measurement_count}")
             print(f"Completion: {100 * t / N:.2f}%")
@@ -129,10 +129,9 @@ def run_simulation():
                 ekf.measurement(z, landmark_bearing_sensor, data_manager)
             else:
                 ekf.no_measurement()
-        else: 
+        else:
             ekf.no_measurement()
 
-        
         error.append(ekf.r_m - next_state[0:3])
 
     if type(landmark_bearing_sensor) == SimulatedMLLandmarkBearingSensor:
@@ -143,32 +142,6 @@ def run_simulation():
     plt.plot(error)
     plt.show()
     # TODO: IMU runs at a higher rate than the rest of the system so probably better to introduce a separate dt for it
-
-    # # Run the simulation
-    # for i in range(timesteps):
-
-    #     # Get the measurements
-    #     # Take the satellite position in ECEF coordinates for the measurement
-    #     # Ignore the actual attitude of the satellite in the determination of visible landmarks
-    #     # visible_landmarks = visible_landmarks_list(ground_truth[i + 1, :3], landmark_test_objects)
-    #     # z_landmark = measure_z_landmark(
-    #     #     ground_truth[i + 1, :3], ground_truth_quat[i + 1, :], visible_landmarks
-    #     # )
-
-    #     landmark_list = []
-    #     for _, landmark in enumerate(visible_landmarks):
-    #         landmark_list.append(landmark.pos)
-    #     landmark_list = np.array(landmark_list)
-    #     z = (z_landmark, landmark_list)
-
-    #     # Run the EKF post-update step
-    #     if i == timesteps - 1:
-    #         print("debug")
-
-    #     ekf.measurement(z)
-    #     if landmark_list.shape[0] > 0:
-    #         print("landmark seen")
-    #     print("error", ekf.r_m - ground_truth[i + 1, :3])
 
 
 if __name__ == "__main__":
