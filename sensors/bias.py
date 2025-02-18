951dc601f883a565458495bf2c834dcaa6fc8c54
Frederik Markus <80588263+fredmarkus@users.noreply.github.com>
2025-02-18 17:43:40 -0500

    Combined State Filter (#63)
    * rebased on main
    
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>
    
    * linter fixes
    
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>
    
    * add more explanation on R dims
    
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>
    
    ---------
    
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- sensors/bias.py
+++ sensors/bias.py
@@ -1,25 +1,43 @@
+"""
+Module implementing sensor bias
+"""
 from math import sqrt
 
 import numpy as np
 
 
 class BiasParams:
-    def __init__(self, initial_bias, sigma_w):
-        """Parameters for a time-varying bias modeled as a random walk
+    def __init__(self, initial_bias: float, sigma_w: float) -> None:
+        """
+        Parameters for a time-varying bias modeled as a random walk
 
         Args:
             initial_bias (float): [units]
-            sigma_w (float): continuous-time power spectral density of additive white noise to time-derivative of bias. [(units/s)/sqrt(Hz)]
+            sigma_w (float): continuous-time power spectral density of additive white noise 
+            to time-derivative of bias. [(units/s)/sqrt(Hz)]
         """
         self.initial_bias = initial_bias
         self.sigma_w = sigma_w
 
-    def get_random_params(initial_bias_range, sigma_w_range):
+    @staticmethod
+    def get_random_params(
+        initial_bias_range: list, sigma_w_range: list
+    ) -> "BiasParams":
+        """
+        Getter for random bias parameters
+
+        Args:
+            initial_bias_range (list): [min, max]
+            sigma_w_range (list): [min, max]
+
+        Returns:
+            BiasParams: bias parameters
+        """
         return BiasParams(np.random.uniform(*initial_bias_range), np.random.uniform(*sigma_w_range))
 
 
 class Bias:
-    def __init__(self, dt, bias_params):
+    def __init__(self, dt: float, bias_params: BiasParams) -> None:
         """Initialize a time-varying bias modeled as a random walk
 
         Args:
@@ -32,10 +50,16 @@ class Bias:
         # discrete version of sigma_w causing the bias to random walk when integrated
         self.sigma_random_walk_ = bias_params.sigma_w / sqrt(dt)
 
-    def update(self):
+    def update(self) -> float:
+        """
+        Update the bias
+        """
         noise = self.sigma_random_walk_ * np.random.standard_normal()
         self.bias += self.dt * noise
         return self.bias
 
-    def get_bias(self):
+    def get_bias(self) -> float:
+        """
+        Getter for the bias
+        """
         return self.bias
--- sensors/sensor.py
+++ sensors/sensor.py
@@ -1,12 +1,15 @@
+"""
+Module that implements a sensor class that adds noise to a clean signal.
+"""
 from math import sqrt
 
 import numpy as np
 
-from sensors.bias import Bias
+from sensors.bias import Bias, BiasParams
 
 
 class SensorNoiseParams:
-    def __init__(self, biasParams, sigma_v, scale_factor_error):
+    def __init__(self, biasParams: BiasParams, sigma_v: float, scale_factor_error: float) -> None:
         """Parameters for a time-varying bias modeled as a random walk
 
         Args:
@@ -18,7 +21,21 @@ class SensorNoiseParams:
         self.sigma_v = sigma_v
         self.scale_factor_error = scale_factor_error
 
-    def get_random_params(biasParams, sigma_v_range, scale_factor_error_range):
+    @staticmethod
+    def get_random_params(
+        biasParams: BiasParams, sigma_v_range: list, scale_factor_error_range: list
+    ) -> "SensorNoiseParams":
+        """
+        Getter for random bias parameters
+
+        Args:
+            biasParams (BiasParams): bias parameters
+            sigma_v_range (list): [min, max]
+            scale_factor_error_range (list) [min, max]
+
+        Returns:
+            SensorNoiseParams: sensor noise parameters
+        """
         return SensorNoiseParams(
             biasParams,
             np.random.uniform(*sigma_v_range),
@@ -27,7 +44,14 @@ class SensorNoiseParams:
 
 
 class Sensor:
-    def __init__(self, dt, sensor_noise_params):
+    def __init__(self, dt: float, sensor_noise_params: SensorNoiseParams) -> None:
+        """
+        Sensor class that adds noise to a clean signal.
+
+        Args:
+            dt (float): The time step for the simulation.
+            sensor_noise_params (SensorNoiseParams): The noise parameters for the sensor.
+        """
         self.dt = dt
         self.bias = Bias(dt, sensor_noise_params.bias_params)
 
@@ -36,26 +60,57 @@ class Sensor:
 
         self.scale_factor_error = sensor_noise_params.scale_factor_error
 
-    def update(self, clean_signal):
+    def update(self, clean_signal: np.ndarray) -> np.ndarray:
+        """
+        Update the measurements of the sensor by applying noise to the clean signal.
+
+        Args:
+            clean_signal (np.ndarray): The clean signal.
+
+        Returns:
+            np.ndarray: The noisy signal.
+        """
         self.bias.update()
         noise = self.white_noise * np.random.standard_normal()
         return (1 + self.scale_factor_error) * clean_signal + self.bias.get_bias() + noise
 
-    def get_bias(self):
+    def get_bias(self) -> float:
+        """
+        Getter for the bias
+        """
         return self.bias.get_bias()
 
 
 class TriAxisSensor:
-    def __init__(self, dt, axes_params):
+    def __init__(self, dt: float, axes_params: list) -> None:
+        """
+        Class that creates a noisy tri-axis signal.
+
+        Args:
+            dt (float): The time step for the simulation.
+            axes_params (IMUNoiseParams): The noise parameters for the sensor.
+        """
         self.dt = dt
         self.x = Sensor(dt, axes_params[0])
         self.y = Sensor(dt, axes_params[1])
         self.z = Sensor(dt, axes_params[2])
 
-    def get_bias(self):
+    def get_bias(self) -> np.ndarray:
+        """
+        Getter for the bias
+        """
         return np.array([self.x.get_bias(), self.y.get_bias(), self.z.get_bias()])
 
-    def update(self, clean_signal):
+    def update(self, clean_signal: np.ndarray) -> np.ndarray:
+        """
+        Update the measurements of the TriAxisSensor by applying noise to the clean signal.
+
+        Args:
+            clean_signal (np.ndarray): The clean signal.
+
+        Returns:
+            np.ndarray: The noisy signal.
+        """
         return np.array(
             [
                 self.x.update(clean_signal[0]),
