e137efe408e2e4d38d3f4d59de8e1c9378e7919c
Amaar Quadri <amaarquadri@gmail.com>
2025-02-21 07:04:21 -0500

    disable caching to generate random images each time, variable naming

--- VisionTrainingGround/DataPipeline/generate_training_data.py
+++ VisionTrainingGround/DataPipeline/generate_training_data.py
@@ -1,4 +1,4 @@
-from image_simulation.earth_vis import EarthImageSimulator
+from image_simulation.earth_vis import EarthImageSimulator, GeoTIFFCache
 from sensors.camera_model import CameraModelManager
 from utils.earth_utils import ecef_to_lat_lon, lat_lon_to_ecef, get_nadir_rotation
 from scipy.spatial.transform import Rotation
@@ -6,14 +6,12 @@ from scipy.spatial.transform import Rotation
 
 IMAGES_PER_REGION = 1000
 NOMINAL_ALTITUDE = 510e3
-ALTITUDE_VARIANCE = 20e3
-
+ALTITUDE_VARIATION = 20e3
 
 
 def main():
-    image_simulator = EarthImageSimulator()
+    image_simulator = EarthImageSimulator(GeoTIFFCache(max_cache_size=0))
     camera_manager = CameraModelManager()
-
 
 
 if __name__ == "__main__":
