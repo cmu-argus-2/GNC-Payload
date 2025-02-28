pushing Jacobian fix to correct branch
Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

orbit_determination/ekf.py
@@ -107,21 +107,20 @@ class EKF:
         x_new = f(x, self.dt)
 
         self.q_p = left_q(self.q_m) @ quaternion.as_float_array(
-            quaternion.from_rotation_vector(0.5 * self.dt * (wf - self.w_b))
+            quaternion.from_rotation_vector(self.dt * (wf - self.w_b))
         )
 
         self.r_p = x_new[0:3]
         self.v_p = x_new[3:6]
 
         dqdq = quaternion.as_rotation_matrix(
-            quaternion.from_rotation_vector(-0.5 * self.dt * (wf - self.w_b))
+            quaternion.from_rotation_vector(-1 * self.dt * (wf - self.w_b))
         )
         dqdw = (
-            -0.5
-            * self.dt
+            self.dt
             * G(self.q_p).T
             @ left_q(self.q_m)
-            @ Drp2q(0.5 * self.dt * (wf - self.w_b))
+            @ Drp2q(self.dt * (wf - self.w_b))
         )
 
         A = np.block(
@@ -201,8 +200,12 @@ class EKF:
             self.r_m = np.array(x_p[0:3]) + delta[0:3]
             self.v_m = np.array(x_p[3:6]) + delta[3:6]
             self.q_m = quaternion.as_rotation_vector(
-                quaternion.from_rotation_vector(np.array(x_p[6:9]))
-                * quaternion.from_rotation_vector(delta[6:9])
+                quaternion.as_quat_array(
+                    left_q(quaternion.as_float_array(quaternion.from_rotation_vector(x_p[6:9])))
+                    @ quaternion.as_float_array(
+                        quaternion.from_rotation_vector(np.array(delta[6:9]))
+                    )
+                )
             )
             self.w_b = np.array(x_p[9:12]) + delta[9:12]
 

orbit_determination/test_ekf.py
@@ -93,7 +93,7 @@ def run_simulation() -> None:
     num_iter = 5
 
     # Fix a constant rotation velocity for the test.
-    rot = np.array([0, 0, np.pi / 3])
+    rot = np.array([0, 0, np.pi / 18])
 
     # Initialize IMU and EKF
     imu = imu_init(dt)
@@ -102,7 +102,7 @@ def run_simulation() -> None:
         # TODO: Apply initial error to quaternion initialization
         # error ranges are in meters and m/s
         r=initial_state[0:3] + np.random.normal(0, 500, 3),
-        v=initial_state[3:6] + np.random.normal(0, 500, 3),
+        v=initial_state[3:6] + np.random.normal(0, 10, 3),
         q=quaternion.as_float_array(quaternion.from_rotation_matrix(init_rot)),
         P=np.eye(12) * 10,
         Q=np.eye(12) * 1e-12,
@@ -124,7 +124,7 @@ def run_simulation() -> None:
 
         next_state = f(x, dt)
         next_quat = quaternion.from_rotation_matrix(q) * quaternion.from_rotation_vector(
-            (w - bias) * dt * 0.5
+            (w - bias) * dt
         )
 
         data_manager.push_next_state(next_state, quaternion.as_rotation_matrix(next_quat))

utils/math_utils.py
@@ -110,18 +110,17 @@ def right_q(q: np.ndarray) -> np.ndarray:
 
 def Drp2q(phi: np.ndarray) -> np.ndarray:
     """
-    Compute the derivative of a 3D rotation vector to quaternion mapping.
+    Compute the derivative of the rotation vector to quaternion mapping.
     Args:
         phi (np.ndarray): The rotation vector.
     Returns:
-        np.ndarray: The derivative.
+        np.ndarray: The derivative of the rotation vector to quaternion mapping.
     """
-    frac1 = 1 / (np.sqrt(1 + np.dot(phi, phi)))
-    H1 = np.concatenate([[np.zeros(3)], np.eye(3)], axis=0)
-    frac2 = (1 + np.dot(phi, phi)) ** (-3 / 2)
-    H2 = np.outer(np.append(1, phi), phi)
+    theta = np.linalg.norm(phi) 
+    term1 = -0.5 * np.sin(theta/2) * phi.T / theta
+    term2 = 0.5 * np.cos(theta/2) * np.outer(phi/theta,phi/theta) + np.sin(theta/2) * (np.eye(3)/theta - np.outer(phi,phi)/theta**3)
 
-    return frac1 * H1 - frac2 * H2
+    return np.block([[term1], [term2]])
 
 
 def G(q: np.ndarray) -> np.ndarray: