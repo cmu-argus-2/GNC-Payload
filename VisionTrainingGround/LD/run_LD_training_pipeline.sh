7ca65a44f08368597180e6044de43c72a428da52
Amaar Quadri <amaarquadri@gmail.com>
2025-03-16 13:02:23 -0400

    remove temporary echo command

--- VisionTrainingGround/LD/run_LD_training_pipeline.sh
+++ VisionTrainingGround/LD/run_LD_training_pipeline.sh
@@ -2,7 +2,6 @@
 
 regions="05V 09V 10S 10T 11R 12R 14Q 15V 16T 18Q 18S 19J 21H 23L 29Q 30U 32S 32T 33K
 33S 33T 35J 36L 37Q 38K 39P 40R 42R 46Q 48M 49S 50M 51J 52S 53L 54S 54U 55J 57V 59G"
-echo $regions
 
 python run_saliency_analysis.py \
   --regions $regions \
