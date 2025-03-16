a7f92ef4c1514ba851ef19ed5eaa287d6435f538
Amaar Quadri <amaarquadri@gmail.com>
2025-03-15 21:44:14 -0400

    extract generate_training_image function

--- VisionTrainingGround/DataPipeline/generate_training_data.py
+++ VisionTrainingGround/DataPipeline/generate_training_data.py
@@ -106,6 +106,89 @@ def setup_output_directory(output_dir: str, overwrite: bool) -> bool:
     return True
 
 
+def generate_training_image(
+        geotiff_folder: str,
+        output_dir: str,
+        region: str,
+        file_prefix: str,
+        lat_lon_buffer: float,
+        nominal_altitude: float,
+        altitude_variation: float,
+        off_nadir_variation: float,
+) -> None:
+    """
+    Generate a single training image using the EarthImageSimulator.
+
+    This function generates 3 files:
+    - A PNG image file containing the training image.
+    - A .npy file with dtype=str containing the MGRS regions for each pixel.
+    - A .npy file with containing the lat/lon coordinates for each pixel.
+
+    :param geotiff_folder: The path to the folder containing GeoTIFF files for each region.
+    :param output_dir: The path to save the training image.
+    :param region: The MGRS region to generate the training image for.
+    :param file_prefix: The prefix for the output files.
+    :param lat_lon_buffer: The extra buffer in possible lat/lon coordinates for the region.
+    :param nominal_altitude: The nominal altitude of the satellite, in m.
+    :param altitude_variation: The variation in altitude, in m. The actual altitude will be in the range
+                               [nominal_altitude - altitude_variation, nominal_altitude + altitude_variation].
+    :param off_nadir_variation: The maximum variation in off-nadir angle, in degrees.
+    """
+    min_lon, min_lat, max_lon, max_lat = get_MGRS_grid()[region]
+    min_lon -= lat_lon_buffer
+    min_lat -= lat_lon_buffer
+    max_lon += lat_lon_buffer
+    max_lat += lat_lon_buffer
+
+    lat = np.random.uniform(min_lat, max_lat)
+    lon = np.random.uniform(min_lon, max_lon)
+    lat = np.clip(lat, -90, 90)
+    lon = np.clip(lon, -180, 180)
+    altitude = nominal_altitude + np.random.uniform(
+        -altitude_variation, altitude_variation
+    )
+
+    ecef_position = lat_lon_to_ecef(lat, lon)
+    ecef_position /= (R_EARTH + altitude) / np.linalg.norm(ecef_position)
+    ecef_velocity = np.array([0, 0, 1])
+
+    camera_manager = CameraModelManager()
+    perturbed_camera_R_nominal_camera = Rotation.from_euler(
+        "ZX",
+        [np.random.uniform(0, 360), np.random.uniform(0, off_nadir_variation)],
+        degrees=True,
+    ).as_matrix()
+    nominal_body_R_nominal_camera = perturbed_body_R_perturbed_camera = camera_manager[
+        "x+"
+    ].body_R_camera
+    ecef_R_nominal_body = get_nadir_rotation(np.concatenate((ecef_position, ecef_velocity)))
+    ecef_R_perturbed_body = (
+        ecef_R_nominal_body
+        @ nominal_body_R_nominal_camera
+        @ perturbed_camera_R_nominal_camera.T
+        @ perturbed_body_R_perturbed_camera.T
+    )
+
+    image_simulator = EarthImageSimulator(
+        GeoTIFFCache(geotiff_folder=geotiff_folder, max_cache_size=0)
+    )
+    frame, mgrs_regions, lat_lon = image_simulator.simulate_image_for_training(
+        ecef_position, ecef_R_perturbed_body, camera_manager["x+"]
+    )
+
+    region_dir = os.path.join(output_dir, region)
+    os.makedirs(region_dir, exist_ok=True)
+    cv2.imwrite(
+        os.path.join(region_dir, f"{file_prefix}.png"),
+        cv2.cvtColor(frame.image, cv2.COLOR_RGB2BGR),
+    )
+    np.save(
+        os.path.join(region_dir, f"{file_prefix}{MGRS_REGIONS_OUTPUT_FILE_SUFFIX}"),
+        mgrs_regions,
+    )
+    np.save(os.path.join(region_dir, f"{file_prefix}{LAT_LON_OUTPUT_FILE_SUFFIX}"), lat_lon)
+
+
 def main() -> None:
     """
     Generate training data using the EarthImageSimulator.
@@ -118,64 +201,19 @@ def main() -> None:
         return
     regions = list(set(args.regions) - set(args.skip_regions))
 
-    image_simulator = EarthImageSimulator(
-        GeoTIFFCache(geotiff_folder=args.geotiff_folder, max_cache_size=0)
-    )
-    camera_manager = CameraModelManager()
-    grid = get_MGRS_grid()
-    ecef_velocity = np.array([0, 0, 1])
-
     for region in regions:
-        region_dir = os.path.join(args.output_dir, region)
-        os.makedirs(region_dir)
-
-        min_lon, min_lat, max_lon, max_lat = grid[region]
-        min_lon -= args.lat_lon_buffer
-        min_lat -= args.lat_lon_buffer
-        max_lon += args.lat_lon_buffer
-        max_lat += args.lat_lon_buffer
         for i in range(args.num_images):
-            lat = np.random.uniform(min_lat, max_lat)
-            lon = np.random.uniform(min_lon, max_lon)
-            lat = np.clip(lat, -90, 90)
-            lon = np.clip(lon, -180, 180)
-            altitude = args.nominal_altitude + np.random.uniform(
-                -args.altitude_variation, args.altitude_variation
-            )
-
-            ecef_position = lat_lon_to_ecef(lat, lon)
-            ecef_position /= (R_EARTH + altitude) / np.linalg.norm(ecef_position)
-
-            perturbed_camera_R_nominal_camera = Rotation.from_euler(
-                "ZX",
-                [np.random.uniform(0, 360), np.random.uniform(0, args.off_nadir_variation)],
-                degrees=True,
-            ).as_matrix()
-            nominal_body_R_nominal_camera = perturbed_body_R_perturbed_camera = camera_manager[
-                "x+"
-            ].body_R_camera
-            ecef_R_nominal_body = get_nadir_rotation(np.concatenate((ecef_position, ecef_velocity)))
-            ecef_R_perturbed_body = (
-                ecef_R_nominal_body
-                @ nominal_body_R_nominal_camera
-                @ perturbed_camera_R_nominal_camera.T
-                @ perturbed_body_R_perturbed_camera.T
-            )
-
-            frame, mgrs_regions, lat_lon = image_simulator.simulate_image_for_training(
-                ecef_position, ecef_R_perturbed_body, camera_manager["x+"]
-            )
-
             file_prefix = f"{i:05d}"
-            cv2.imwrite(
-                os.path.join(region_dir, f"{file_prefix}.png"),
-                cv2.cvtColor(frame.image, cv2.COLOR_RGB2BGR),
+            generate_training_image(
+                args.geotiff_folder,
+                args.output_dir,
+                region,
+                file_prefix,
+                args.lat_lon_buffer,
+                args.nominal_altitude,
+                args.altitude_variation,
+                args.off_nadir_variation,
             )
-            np.save(
-                os.path.join(region_dir, f"{file_prefix}{MGRS_REGIONS_OUTPUT_FILE_SUFFIX}"),
-                mgrs_regions,
-            )
-            np.save(os.path.join(region_dir, f"{file_prefix}{LAT_LON_OUTPUT_FILE_SUFFIX}"), lat_lon)
 
 
 if __name__ == "__main__":
