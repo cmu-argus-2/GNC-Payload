1d164fbeb2f9e1597c6ed38282dcc063496f7232
Amaar Quadri <amaarquadri@gmail.com>
2025-03-19 16:20:28 -0400

    account for differing dataset section ids

--- image_simulation/blue_marble_downloader.py
+++ image_simulation/blue_marble_downloader.py
@@ -4,7 +4,7 @@ from itertools import product
 
 
 OUTPUT_DIR = os.path.join(__file__, "../blue_marble")
-BASE_URL = "https://eoimages.gsfc.nasa.gov/images/imagerecords/74000"
+BASE_URL = "https://eoimages.gsfc.nasa.gov/images/imagerecords"
 DATASET_IDS_BY_MONTH = ["73938", "73967", "73992", "74017", "74042", "76487", "74092", "74117", "74142", "74167", "74192", "74218"]
 MONTH_NAMES = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
 RESOLUTION = "3x21600x21600"
@@ -14,8 +14,9 @@ def main():
     for i, (month_name, dataset_id) in enumerate(zip(MONTH_NAMES, DATASET_IDS_BY_MONTH)):
         month_dir = os.path.join(OUTPUT_DIR, month_name)
         os.makedirs(month_dir, exist_ok=True)
+        dataset_section_id = dataset_id[:2] + "000"
         for letter, number in product("ABCD", range(1, 3)):
-            url = f"{BASE_URL}/{dataset_id}/world.2004{(i + 1):02d}.{RESOLUTION}.{letter}{number}.png"
+            url = f"{BASE_URL}/{dataset_section_id}/{dataset_id}/world.2004{(i + 1):02d}.{RESOLUTION}.{letter}{number}.png"
             response = requests.get(url, stream=True)
             with open(os.path.join(month_dir, f"{letter}{number}.png"), "wb") as file:
                 file.write(response.content)
