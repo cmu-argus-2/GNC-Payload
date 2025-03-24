8351d55faf6e5f1ca5cd63feeadec587849a1285
Amaar Quadri <amaarquadri@gmail.com>
2025-03-23 23:44:25 -0400

    lat_lon_to_ecef bug fix

--- VisionTrainingGround/DataPipeline/generate_training_data.py
+++ VisionTrainingGround/DataPipeline/generate_training_data.py
@@ -165,7 +165,7 @@ def generate_training_image(
     lon = np.clip(lon, -180, 180)
     altitude = nominal_altitude + np.random.uniform(-altitude_variation, altitude_variation)
 
-    ecef_position = lat_lon_to_ecef(lat, lon)
+    ecef_position = lat_lon_to_ecef(np.array([lat, lon]))
     ecef_position /= (R_EARTH + altitude) / np.linalg.norm(ecef_position)
     ecef_velocity = np.array([0, 0, 1])
 
