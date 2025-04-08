a0acbc9ad087b3ea6074996d24771d8fab886c54
Amaar Quadri <amaarquadri@gmail.com>
2025-04-08 18:16:33 -0400

    bug fix: specify index so sizes match

--- VisionTrainingGround/LD/run_saliency_analysis.py
+++ VisionTrainingGround/LD/run_saliency_analysis.py
@@ -186,7 +186,7 @@ def generate_saliency_map(
         np.add.at(region_saliency_map_counts, (vs[valid_mask], us[valid_mask]), 1)
 
     nonzero_mask = region_saliency_map_counts > 0
-    region_saliency_map.image_data[nonzero_mask, :] /= region_saliency_map_counts[nonzero_mask]
+    region_saliency_map.image_data[nonzero_mask, 0] /= region_saliency_map_counts[nonzero_mask]
     return region_saliency_map
 
 
